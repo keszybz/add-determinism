d5e06572e8185b004d18090126909ea824806940
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-03-15 17:34:09 +0100

    Also split out processors submodule

---
+++ src/processors/mod.rs
@@ -0,0 +1,40 @@
+mod ar;
+
+use anyhow::Result;
+use log::{debug, warn};
+use std::path::Path;
+use crate::options;
+
+pub struct Processor {
+    filter: fn(&walkdir::DirEntry) -> Result<bool>,
+    process: fn(&options::Options, &Path) -> Result<()>,
+}
+
+const PROCESSORS: [Processor; 1] = [
+    Processor { filter: ar::filter, process: ar::process },
+];
+
+pub fn process_file_or_dir(options: &options::Options, input_path: &Path) -> Result<()> {
+    debug!("Looking at path {:?}…", input_path);
+
+    for entry in walkdir::WalkDir::new(input_path)
+        .follow_links(false)
+        .into_iter()
+        .filter_map(|e| e.ok()) {
+
+            let metadata = entry.metadata()?;
+            if !metadata.is_file() {
+                continue;
+            }
+
+            for processor in PROCESSORS {
+                if (processor.filter)(&entry)? {
+                    (processor.process)(options, entry.path()).unwrap_or_else(|err| {
+                        warn!("Failed to process file: {}", err);
+                    });
+                }
+            }
+        }
+
+    Ok(())
+}
