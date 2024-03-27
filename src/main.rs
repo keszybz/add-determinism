6afaaac258fb889b5ca1984926dc498b573287ca
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-03-27 17:46:44 +0100

    Support running as brp helper

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,45 @@ mod handlers;
 mod options;
 mod simplelog;
 
-use anyhow::Result;
-use log::warn;
+use anyhow::{Result, anyhow};
+use log::{debug, warn};
+use std::env;
+use std::path::Path;
+
+fn brp_check() -> Result<()> {
+    // env::current_exe() does readlink("/proc/self/exe"), which returns
+    // the target binary, so we cannot use that.
+
+    let arg0 = env::args().next().unwrap();
+
+    let brp = Path::new(&arg0)
+        .file_name()
+        .ok_or(anyhow!("Exe path doesn't have a file name?"))?
+        .to_str()
+        .ok_or(anyhow!("Exe file name is not valid unicode"))?
+        .starts_with("brp-");
+
+    debug!("Running as {}… (brp={})", arg0, if brp { "true" } else { "false" });
+
+    if brp {
+        let build_root = env::var("RPM_BUILD_ROOT")
+            .map_err(|_| anyhow!("RPM_BUILD_ROOT variable is not defined"))?;
+
+        if build_root.is_empty() || build_root == "/" {
+            return Err(anyhow!("RPM_BUILD_ROOT={:?} is not allowed", build_root));
+        }
+    }
+
+    Ok(())
+}
 
 fn main() -> Result<()> {
     let config = match options::Config::make()? {
         None => { return Ok(()); },
         Some(some) => some
     };
+
+    brp_check()?;
 
     let mut inodes_seen = handlers::inodes_seen();
 
