88ff0bdaa5985c91514f785bb09d5bdf33a6b077
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-06-08 13:21:15 +0200

    tests: run all the marshalparser tests
    The tests are generated in build.rs
    (solution taken from https://stackoverflow.com/a/34666891).
    I think that this is nicer than globbing internally some test function because
    we get the usual parallelization and ability to call tests individually.

--- tests/test_handlers/test_pyc.rs
+++ tests/test_handlers/test_pyc.rs
@@ -2,6 +2,7 @@
 
 use std::fs;
 use std::fs::File;
+use std::io::Read;
 use std::os::linux::fs::MetadataExt;
 use std::path::Path;
 
@@ -99,3 +100,36 @@ fn test_testrelro_fixed() {
     assert_eq!(orig.modified().unwrap(), new.modified().unwrap());
     assert_eq!(orig.st_ino(), new.st_ino());
 }
+
+fn test_python_stdlib_file(input_pyc: &str) {
+    let input_pyc = Path::new(input_pyc);
+    let (_dir, input) = prepare_dir(input_pyc.to_str().unwrap()).unwrap();
+
+    let fixed_pyc = input_pyc.with_extension("pyc.fixed");
+    let have_mod = fixed_pyc.exists();
+
+    let pyc = make_handler(1717842014, pyc::Pyc::boxed).unwrap();
+
+    assert!(pyc.filter(&*input).unwrap());
+    assert_eq!(pyc.process(&*input).unwrap(), have_mod);
+
+    let mut data_expected = vec![];
+    File::open(
+        if have_mod { &fixed_pyc } else { input_pyc }
+    ).unwrap()
+        .read_to_end(&mut data_expected).unwrap();
+
+    let mut data_output: Vec<u8> = vec![];
+    File::open(&*input).unwrap()
+        .read_to_end(&mut data_output).unwrap();
+
+    assert_eq!(data_output, data_expected);
+}
+
+#[test]
+fn test_python_stdlib_file_1() {
+    // Let's call test_python_stdlib_file() one manually for easier development.
+    test_python_stdlib_file("tests/cases/python_stdlib/3.9/sunau.cpython-39.pyc");
+}
+
+include!(concat!(env!("OUT_DIR"), "/test_python_stdlib_cases.rs"));
