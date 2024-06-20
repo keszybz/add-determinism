b00e0a725d13456b85e06cc2cc1642509ea9db3c
Zbigniew Jędrzejewski-Szmek <zbyszek@in.waw.pl>
2024-06-20 22:32:24 +0200

    tests: add test case with invalid utf-8

--- tests/test_handlers/test_javadoc.rs
+++ tests/test_handlers/test_javadoc.rs
@@ -46,3 +46,21 @@ fn test_javadoc_fixed() {
     assert_eq!(orig.modified().unwrap(), new.modified().unwrap());
     assert_eq!(orig.st_ino(), new.st_ino());
 }
+
+#[test]
+fn test_invalid_utf8() {
+    let (_dir, input) = prepare_dir("tests/cases/invalid-utf8.html").unwrap();
+
+    let javadoc = make_handler(1704106800, javadoc::Javadoc::boxed).unwrap();
+
+    assert!(javadoc.filter(&*input).unwrap());
+
+    let orig = input.metadata().unwrap();
+
+    assert_eq!(javadoc.process(&*input).unwrap(), false);
+
+    let new = input.metadata().unwrap();
+    assert_eq!(orig.created().unwrap(), new.created().unwrap());
+    assert_eq!(orig.modified().unwrap(), new.modified().unwrap());
+    assert_eq!(orig.st_ino(), new.st_ino());
+}
